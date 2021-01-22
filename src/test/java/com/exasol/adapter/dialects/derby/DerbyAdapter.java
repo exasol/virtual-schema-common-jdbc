9c77c101e84cfed02044057ad7ec0d39be893dd7
Sebastian Bär <sebastian.baer@exasol.com>
2021-01-22 11:55:21 +0100

    #86: Removed superfluous `implements` in DerbyAdapter.

--- src/test/java/com/exasol/adapter/dialects/derby/DerbyAdapter.java
+++ src/test/java/com/exasol/adapter/dialects/derby/DerbyAdapter.java
@@ -1,7 +1,6 @@
 package com.exasol.adapter.dialects.derby;
 
 import com.exasol.adapter.AdapterProperties;
-import com.exasol.adapter.VirtualSchemaAdapter;
 import com.exasol.adapter.dialects.SqlDialect;
 import com.exasol.adapter.jdbc.AbstractJdbcAdapter;
 import com.exasol.adapter.jdbc.ConnectionFactory;
@@ -12,7 +11,7 @@ import com.exasol.adapter.jdbc.ConnectionFactory;
  * This adapter is intended for integration tests of the JDBC Virtual Schema adapter only.
  * </p>
  */
-public class DerbyAdapter extends AbstractJdbcAdapter implements VirtualSchemaAdapter {
+public class DerbyAdapter extends AbstractJdbcAdapter {
     @Override
     protected SqlDialect createDialect(final ConnectionFactory connectionFactory, final AdapterProperties properties) {
         return new DerbySqlDialect(connectionFactory, properties);
