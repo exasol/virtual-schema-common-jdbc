3ee6a6902c5c054d6cacd0a5ef6ea889adf7864f
KK <christoph.kuhnke@exasol.com>
2023-03-09 15:54:10 +0100

    fixed sonar findings and javadoc

--- src/test/java/com/exasol/adapter/jdbc/WildcardsTest.java
+++ src/test/java/com/exasol/adapter/jdbc/WildcardsTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class WildcardsTest {
-
+class WildcardsTest {
     @ParameterizedTest
     @CsvSource(value = { //
             "abc, abc", //
